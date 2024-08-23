944bd3316a721567a5b0cd6df5082bf1dffb968a
Djawad-boutemine <sleepwell@pop-os.localdomain>
2024-08-23 15:29:33 +0100

    adding backend link

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/:path*", // Proxy to backend server
+        destination: "https://audiophile-back-20jv.onrender.com/:path*", // Proxy to backend server
       },
     ];
   },
